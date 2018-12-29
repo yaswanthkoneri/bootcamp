8eed8c860c69f856d487ec33127a5fbaed35c773
Faisal Ali Siddiqui <faisal.siddiqui@capillarytech.com>
2018-12-29 13:21:59 +0530

    updated closures and setTimeout

--- closure.js
+++ closure.js
@@ -31,3 +31,25 @@ var addToFour = new addTo(4);
 
 console.log(addToThree(1));//4 
 console.log(addToFour(1));//4 
+
+// Javascript has lexical scoping which means outer scope elements are available in inner scope
+let f;
+if(true) {
+    let i = 1;
+    f = () => {
+        console.log(i);
+    }
+}
+console.log(i); // refernce error
+f(); // 1
+console.dir(f); // in [[scopes]] array, we will have global and block(i=1)
+
+let f = () => {
+    let i = 1;
+    let j = 2;
+    return () => {
+        console.log(i);
+    }
+}
+console.dir(f); // in [[scopes]] array, we will have global and closure(with i=1 and no j, as its not used)
+
--- setTimeout.js
+++ setTimeout.js
@@ -7,3 +7,52 @@ console.log("third")
 
 
 console.log("first");setTimeout(function() { console.log("second");}, 0);console.log("third");
+
+
+//..........................................
+for(let i=0; i<3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000);
+}
+console.log('done');
+
+/**
+ * outputs: done
+ * 0
+ * 1
+ * 2
+ */
+
+for(var i=0; i<3; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000);
+}
+console.log('done');
+
+/**
+ * outputs: done
+ * 3
+ * 3
+ * 3
+ */
+
+// this is due to closure of i in setTimeout function 
+// to solve this useing var, use iffe
+
+for(var i=0; i<3; i++) {
+    ((i) => {
+        setTimeout(() => {
+            console.log(i);
+        }, 1000)
+    })(i);
+}
+console.log('done');
+
+/**
+ * outputs: done
+ * 0
+ * 1
+ * 2
+ */
